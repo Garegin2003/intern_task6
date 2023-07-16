f63d48fa03892c323f7eb1b38dc2f3bbd890e1f4
Garegin2003 <garehin.hakobyan.s@gmail.com>
2023-07-16 12:48:50 +0300

    finish functional

--- script.js
+++ script.js
@@ -1,38 +1,102 @@
 const uploadBtn = document.querySelector('.upload-btn');
+const progress = document.querySelector('.container');
+const input = document.querySelector('.drop-area__item');
+const dropArea = document.querySelector('.drop-area');
 
+let percent = 0;
+let fileIndex = 0;
+let step = 1;
 
 uploadBtn.addEventListener('click', uploadFiles);
+dropArea.addEventListener('dragover', handleDragOver);
+dropArea.addEventListener('dragleave', handleDragLeave);
+dropArea.addEventListener('drop', handleDrop);
+
+function handleDragOver(e) {
+  e.preventDefault();
+  dropArea.classList.add('drag-over');
+}
+
+function handleDragLeave(e) {
+  e.preventDefault();
+  dropArea.classList.remove('drag-over');
+}
+
+function handleDrop(e) {
+  e.preventDefault();
+  dropArea.classList.remove('drag-over');
+  const files = e.dataTransfer.files;
+  input.files = files;
+}
 
 function uploadFiles() {
-  const files = document.querySelector('input[type=file]').files;
   
+  const files = input.files;
 
   if (files.length === 0) {
-    alert('Please select files to upload.');
     return;
   }
-  
 
-  const formData = new FormData();
+  const maxParallelUploads = 40;
+  const startIndex = (step - 1) * maxParallelUploads;
+  const endIndex = Math.min(startIndex + maxParallelUploads, files.length);
 
-  for (let i = 0; i < files.length; i++) {
+  for (let i = startIndex; i < endIndex; i++) {
+    createProgressElement();
+
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
     formData.append('files', files[i]);
+
+    xhr.open('POST', 'http://localhost:3000/upload', true);
+    xhr.upload.addEventListener('progress', handleProgress);
+    xhr.onreadystatechange = handleStateChange;
+    xhr.send(formData);
+
+    input.value = '';
   }
-  
+}
 
-  fetch('http://localhost:8080/upload', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => {
+function createProgressElement() {
+  const progressElement = document.createElement('div');
+  progressElement.classList.add('container__progress');
+  progressElement.innerHTML = `
+    <div style="width: ${percent}%" class="container__item"></div>
+  `;
+  progress.appendChild(progressElement);
+}
 
-    if (response.ok) {
-      console.log('Files uploaded successfully!');
-    } else {
-      console.log('Error uploading files.');
-    }
-  })
-  .catch(error => {
-    console.log('An error occurred while uploading files.');
+function handleProgress(e) {
+  if (e.lengthComputable) {
+    percent = ((e.loaded / e.total) * 100).toFixed(2);
+    updateProgressElementsWidth();
+    console.log(percent);
+  }
+}
+
+function updateProgressElementsWidth() {
+  const progressElements = document.querySelectorAll('.container__item');
+  progressElements.forEach((element) => {
+    element.style.width = percent + '%';
   });
+}
+
+function handleStateChange() {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      console.log('ok');
+    } else {
+      console.log('hajox');
+    }
+
+    if (fileIndex === endIndex - 1) {
+      if (endIndex < files.length) {
+        fileIndex = endIndex;
+        step++;
+        uploadFiles();
+      }
+    }
+
+    fileIndex++;
+  }
 }
--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = 8080;
+const port = 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
