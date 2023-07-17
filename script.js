ec692509a8f7b070804796b5d613e1eaa0a1d362
Garegin2003 <garehin.hakobyan.s@gmail.com>
2023-07-17 11:43:19 +0300

    final function on 3 download

--- script.js
+++ script.js
@@ -1,102 +1,129 @@
 const uploadBtn = document.querySelector('.upload-btn');
 const progress = document.querySelector('.container');
 const input = document.querySelector('.drop-area__item');
-const dropArea = document.querySelector('.drop-area');
+const containerItems = []
 
-let percent = 0;
 let fileIndex = 0;
-let step = 1;
+let step = 0;
 
 uploadBtn.addEventListener('click', uploadFiles);
+
+const dropArea = document.querySelector('.drop-area');
+
 dropArea.addEventListener('dragover', handleDragOver);
 dropArea.addEventListener('dragleave', handleDragLeave);
 dropArea.addEventListener('drop', handleDrop);
 
 function handleDragOver(e) {
+
   e.preventDefault();
   dropArea.classList.add('drag-over');
+
 }
 
 function handleDragLeave(e) {
+
   e.preventDefault();
   dropArea.classList.remove('drag-over');
-}
 
+}
 function handleDrop(e) {
+
   e.preventDefault();
   dropArea.classList.remove('drag-over');
   const files = e.dataTransfer.files;
-  input.files = files;
+
+  const dataTransfer = new DataTransfer();
+
+  Array.from(input.files).forEach(file => {
+    dataTransfer.items.add(file);
+  });
+
+  Array.from(files).forEach(file => {
+    dataTransfer.items.add(file);
+  });
+
+  input.files = dataTransfer.files;
+
 }
 
+
+
 function uploadFiles() {
-  
+
   const files = input.files;
 
-  if (files.length === 0) {
+  if (input.files.length === 0) {
+
     return;
+
   }
 
-  const maxParallelUploads = 40;
-  const startIndex = (step - 1) * maxParallelUploads;
+  const maxParallelUploads = 3;
+  const startIndex = step * maxParallelUploads;
   const endIndex = Math.min(startIndex + maxParallelUploads, files.length);
 
   for (let i = startIndex; i < endIndex; i++) {
-    createProgressElement();
+
+    const containerProgress = document.createElement('div');
+    containerProgress.classList.add('container__progress');
+    containerProgress.innerHTML = '<div class="container__item"><span>0.00</span></div>';
+
+    progress.appendChild(containerProgress);
 
     const xhr = new XMLHttpRequest();
     const formData = new FormData();
+
     formData.append('files', files[i]);
 
     xhr.open('POST', 'http://localhost:3000/upload', true);
-    xhr.upload.addEventListener('progress', handleProgress);
-    xhr.onreadystatechange = handleStateChange;
-    xhr.send(formData);
 
-    input.value = '';
-  }
-}
+    xhr.upload.addEventListener('progress', (e) => {
 
-function createProgressElement() {
-  const progressElement = document.createElement('div');
-  progressElement.classList.add('container__progress');
-  progressElement.innerHTML = `
-    <div style="width: ${percent}%" class="container__item"></div>
-  `;
-  progress.appendChild(progressElement);
-}
+      if (e.lengthComputable) {
+        const loaded = e.loaded;
+        const total = e.total;
+        const percent = ((loaded / total) * 100).toFixed(2);
 
-function handleProgress(e) {
-  if (e.lengthComputable) {
-    percent = ((e.loaded / e.total) * 100).toFixed(2);
-    updateProgressElementsWidth();
-    console.log(percent);
-  }
-}
+        const containerItem = containerProgress.querySelector('.container__item');
 
-function updateProgressElementsWidth() {
-  const progressElements = document.querySelectorAll('.container__item');
-  progressElements.forEach((element) => {
-    element.style.width = percent + '%';
-  });
-}
+        containerItem.style.width = percent + '%';
+        containerItem.innerHTML = `<span class="container__text">${percent}</span>`;
+        
+        if (percent === '100.00') {
+          containerItem.style.backgroundColor = '#437bf5';
+        }
 
-function handleStateChange() {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log('ok');
-    } else {
-      console.log('hajox');
-    }
-
-    if (fileIndex === endIndex - 1) {
-      if (endIndex < files.length) {
-        fileIndex = endIndex;
-        step++;
-        uploadFiles();
       }
-    }
 
-    fileIndex++;
+    });
+
+    xhr.onreadystatechange = () => {
+
+      if (xhr.readyState === 4) {
+
+        if (xhr.status === 200) {
+
+          console.log('ok');
+
+        } else {
+
+          progress.innerHTML = '<p>chdarav</p>'
+          
+        }
+
+        fileIndex++;
+        if (fileIndex === files.length) {
+          step = 0;
+          fileIndex = 0;
+          input.value = ""
+        } else if (fileIndex === endIndex) {
+          step++;
+          uploadFiles();
+        }
+      }
+    };
+
+    xhr.send(formData);
   }
 }
