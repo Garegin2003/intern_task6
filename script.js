74e2baeb2749637db8da32f7f9d2339a45d9191a
Garegin2003 <garehin.hakobyan.s@gmail.com>
2023-07-24 10:14:10 +0300

    uploader

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const uploadBtn = document.querySelector('.upload-btn');
 const progress = document.querySelector('.container');
 const input = document.querySelector('.drop-area__item');
+let maxParallelUploads = 3;
+const files = []
 
 let fileIndex = 0;
 let step = 0;
+let progressContainers = [];
+let uploadStarted = false;
+let arr = []
 
 uploadBtn.addEventListener('click', uploadFiles);
 
@@ -22,14 +27,15 @@ function handleDragLeave(e) {
   e.preventDefault();
   dropArea.classList.remove('drag-over');
 }
+
 function handleDrop(e) {
   e.preventDefault();
   dropArea.classList.remove('drag-over');
-  const files = e.dataTransfer.files;
+  const files = [...e.dataTransfer.files];
 
   const dataTransfer = new DataTransfer();
 
-  Array.from(input.files).forEach((file) => {
+  Array.from(files).forEach((file) => {
     dataTransfer.items.add(file);
   });
 
@@ -40,57 +46,110 @@ function handleDrop(e) {
   input.files = dataTransfer.files;
 }
 
-function uploadFiles() {
-  const files = input.files;
+function createProgressContainer() {
+  const containerProgress = document.createElement('div');
+  containerProgress.classList.add('container__progress');
+
+  const containerItem = document.createElement('div');
+  containerItem.classList.add('container__item');
+
+  const span = document.createElement('span');
+  span.classList.add('container__text');
+  span.textContent = '0.00';
+
+  containerItem.appendChild(span);
+  containerProgress.appendChild(containerItem);
+  progress.appendChild(containerProgress);
+
+  return containerProgress;
+}
+
+function updateProgress(containerProgress, percent) {
+  if (containerProgress) {
+    const containerItem = containerProgress.querySelector('.container__item');
+    const containerText = containerProgress.querySelector('.container__text');
+  
+    containerItem.style.width = percent + '%';
+    containerText.textContent = percent;
+  
+    if (parseFloat(percent) === 100) {
+      containerItem.classList.add('completed');
+    }
+  }
+
+}
+
+ function uploadFiles() {
+  files.push(...input.files);
 
   if (input.files.length === 0) {
     return;
   }
+  input.files = null;
+  input.value = ''
+  console.log(files);
 
-  const existingProgressContainers = document.querySelectorAll(
-    '.container__progress'
-  );
-  const numExistingContainers = existingProgressContainers.length;
 
-  const maxParallelUploads = 3;
-  const startIndex = step * maxParallelUploads;
-  const endIndex = Math.min(startIndex + maxParallelUploads, files.length);
-
-  if (numExistingContainers === 0) {
-    for (let i = 0; i < files.length; i++) {
-      const containerProgress = document.createElement('div');
-      containerProgress.classList.add('container__progress');
-      containerProgress.innerHTML =
-        '<div class="container__item"><span>0.00</span></div>';
-
-      progress.appendChild(containerProgress);
+    const existingProgressContainers = document.querySelectorAll('.container__progress');
+    const numExistingContainers = existingProgressContainers.length;
+    for (let i = numExistingContainers; i < files.length; i++) { 
+      const containerProgress = createProgressContainer();
+      progressContainers.push(containerProgress);
+      arr.push(containerProgress)
     }
+
+
+  if (!uploadStarted) { 
+    
+    // if (arr.length >= 3) {
+    //   maxParallelUploads = 3
+    // }
+    // else if(arr.length ===2) {
+    //   maxParallelUploads = 2
+    // }
+    // else if(arr.length === 1) {
+    //   maxParallelUploads = 1
+    // }
+
+    uploadStarted = true;
+    upload();
+  }
+
+
+}
+function upload() {
+
+
+
+  let startIndex = step * maxParallelUploads;
+  let endIndex = Math.min(startIndex + maxParallelUploads, files.length);
+
+  if (endIndex - startIndex === 0) {
+    console.log('fef');
+    return
+  }
+
+  if (endIndex - startIndex === 1) {
+    endIndex = startIndex + 1;
+  }
+  if (endIndex - startIndex === 2) {
+    endIndex = startIndex + 2
   }
 
   for (let i = startIndex; i < endIndex; i++) {
     const xhr = new XMLHttpRequest();
     const formData = new FormData();
     formData.append('files', files[i]);
-
+    console.log(i);
     xhr.open('POST', 'http://localhost:3000/upload', true);
 
+
     xhr.upload.addEventListener('progress', (e) => {
+    const containerProgress = progressContainers[i]
+
       if (e.lengthComputable) {
-        const loaded = e.loaded;
-        const total = e.total;
-        const percent = ((loaded / total) * 100).toFixed(2);
-        const containerProgress = document.querySelectorAll(
-          '.container__progress'
-        )[i];
-        const containerItem =
-          containerProgress.querySelector('.container__item');
-
-        containerItem.style.width = percent + '%';
-        containerItem.innerHTML = `<span class="container__text">${percent}</span>`;
-
-        if (percent === '100.00') {
-          containerItem.style.backgroundColor = '#437bf5';
-        }
+        const percent = ((e.loaded / e.total) * 100).toFixed(2);
+        updateProgress(containerProgress, percent);
       }
     });
 
@@ -99,17 +158,22 @@ function uploadFiles() {
         if (xhr.status === 200) {
           console.log('ok');
         } else {
-          progress.innerHTML = '<p>chdarav</p>';
+          const containerProgress = progressContainers[i];
+          const containerItem = containerProgress.querySelector('.container__item');
+          containerItem.classList.add('error');
+          containerItem.innerText = 'Upload Failed';
         }
 
         fileIndex++;
         if (fileIndex === files.length) {
           step = 0;
           fileIndex = 0;
-          input.value = '';
+          progressContainers = [];
+          uploadStarted = false
+          return
         } else if (fileIndex === endIndex) {
           step++;
-          uploadFiles();
+          upload();
         }
       }
     };
